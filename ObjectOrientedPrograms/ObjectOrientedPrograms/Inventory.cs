30fa05643881f18e13efcd5eb10858f3c52c9767
ahsan133 <mahsanullah512@gmail.com>
2020-01-02 12:24:47 +0530

    [MD Ahsanullah] Add. DataManagement program is added

---
+++ ObjectOrientedPrograms/ObjectOrientedPrograms/Inventory.cs
@@ -0,0 +1,57 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Inventory.cs" company="Bridgelabz">
+//   Copyright © 2019 Company="BridgeLabz"
+// </copyright>
+// <creator name="MD Ahsanullah"/>
+// ------------------------------------------------------------------------------------------------------------------
+namespace ObjectOrientedPrograms
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// Getter and setter methods
+    /// </summary>
+    public class Inventory
+    {
+        /// <summary>
+        /// The name
+        /// </summary>
+        private string name;
+
+        /// <summary>
+        /// The weight
+        /// </summary>
+        private int weight;
+
+        /// <summary>
+        /// The price
+        /// </summary>
+        private int price;
+
+        /// <summary>
+        /// Gets or sets the price.
+        /// </summary>
+        /// <value>
+        /// The price.
+        /// </value>
+        public int Price { get => this.price; set => this.price = value; }
+
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        /// <value>
+        /// The name.
+        /// </value>
+        public string Name { get => this.name; set => this.name = value; }
+
+        /// <summary>
+        /// Gets or sets the weight.
+        /// </summary>
+        /// <value>
+        /// The weight.
+        /// </value>
+        public int Weight { get => this.weight; set => this.weight = value; }
+    }
+}
--- ObjectOrientedPrograms/ObjectOrientedPrograms/Program.cs
+++ ObjectOrientedPrograms/ObjectOrientedPrograms/Program.cs
@@ -1,12 +1,38 @@
-﻿using System;
-
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="program.cs" company="Bridgelabz">
+//   Copyright © 2019 Company="BridgeLabz"
+// </copyright>
+// <creator name="MD Ahsanullah"/>
+// ------------------------------------------------------------------------------------------------------------------
 namespace ObjectOrientedPrograms
 {
-    class Program
+    using System;
+
+    /// <summary>
+    /// Main function program
+    /// </summary>
+    public class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int choice;
+            Console.WriteLine("1.Data Management");
+            Console.WriteLine("Enter your choice");
+            choice = int.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    DataManagement dataManagement = new DataManagement();
+                    DataManagement.DataManagementObj();
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
