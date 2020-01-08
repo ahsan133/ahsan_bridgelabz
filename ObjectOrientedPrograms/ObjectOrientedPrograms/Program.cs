b8afb441005c81887904024871e040adbef0dc73
ahsan133 <mahsanullah512@gmail.com>
2020-01-08 19:04:48 +0530

    [MD Ahsanullah] add. Stock management program is added

--- ObjectOrientedPrograms/ObjectOrientedPrograms/Program.cs
+++ ObjectOrientedPrograms/ObjectOrientedPrograms/Program.cs
@@ -6,6 +6,7 @@
 // ------------------------------------------------------------------------------------------------------------------
 namespace ObjectOrientedPrograms
 {
+    using ObjectOrientedPrograms.StockAccountManagement;
     using ObjectOrientedPrograms.Company_Shares;
     using ObjectOrientedPrograms.Address_Book;
     using System;
@@ -22,7 +23,7 @@ namespace ObjectOrientedPrograms
         public static void Main(string[] args)
         {
             int choice;
-            Console.WriteLine("1.Data Management\n2.Inventory Management\n3.Deck Of Cards\n4.Address Book\n5.Company Shares");
+            Console.WriteLine("1.Data Management\n2.Inventory Management\n3.Deck Of Cards\n4.Address Book\n5.Company Shares\n6.Stock Account");
             Console.WriteLine("Enter your choice");
             choice = int.Parse(Console.ReadLine());
 
@@ -47,6 +48,10 @@ namespace ObjectOrientedPrograms
                 case 5:
                     CompanyShares CompanyShares = new CompanyShares();
                     CompanyShares.CompanySharesObj();
+                    break;
+                case 6:
+                    StockAccount stockAccount = new StockAccount();
+                    StockAccount.StockAccountObj();
                     break;
                 default:
                     break;
