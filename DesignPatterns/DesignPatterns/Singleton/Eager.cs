213b4ac43314194bf022494526a3d91cde167227
ahsan133 <mahsanullah512@gmail.com>
2020-01-13 13:01:19 +0530

    [MD Ahsanullah] add. Singleton programs added

---
+++ DesignPatterns/DesignPatterns/Singleton/Eager.cs
@@ -0,0 +1,41 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Eager.cs" company="Bridgelabz">
+//   Copyright © 2019 Company="BridgeLabz"
+// </copyright>
+// <creator name="MD Ahsanullah"/>
+// ------------------------------------------------------------------------------------------------------------------
+namespace DesignPatterns.Singleton
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// Eager initialization
+    /// </summary>
+    public class Eager
+    {
+        /// <summary>
+        /// The object
+        /// </summary>
+        private static readonly Eager Obj = new Eager();
+
+        /// <summary>
+        /// Prevents a default instance of the <see cref="Eager"/> class from being created.
+        /// </summary>
+        private Eager()
+        {
+        }
+
+        /// <summary>
+        /// Get objects this instance.
+        /// </summary>
+        /// <returns>
+        /// Returns the object
+        /// </returns>
+        public static Eager Getobject()
+        {
+            return Obj;
+        }
+    }
+}
---
+++ DesignPatterns/DesignPatterns/Singleton/Lazy.cs
@@ -0,0 +1,46 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Lazy.cs" company="Bridgelabz">
+//   Copyright © 2019 Company="BridgeLabz"
+// </copyright>
+// <creator name="MD Ahsanullah"/>
+// ------------------------------------------------------------------------------------------------------------------
+namespace DesignPatterns.Singleton
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// Lazy initialization.
+    /// </summary>
+    public class Lazy
+    {
+        /// <summary>
+        /// Object declaration.
+        /// </summary>
+        private static Lazy obj;
+
+        /// <summary>
+        /// Prevents a default instance of the <see cref="Lazy"/> class from being created.
+        /// </summary>
+        private Lazy()
+        {
+        }
+
+        /// <summary>
+        /// Get objects this instance.
+        /// </summary>
+        /// <returns>
+        /// Returns the object.
+        /// </returns>
+        public static Lazy GetObject()
+        {
+            if (obj == null)
+            {
+                obj = new Lazy();
+            }
+
+            return obj;
+        }
+    }
+}
