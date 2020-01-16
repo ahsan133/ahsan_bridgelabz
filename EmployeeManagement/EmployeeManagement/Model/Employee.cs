6b4cb343ee708c95d9dd3881f52699ac347914ac
ahsan133 <mahsanullah512@gmail.com>
2020-01-16 19:04:15 +0530

    [MD Ahsanullah] add. sql table is created and some functions are added

---
+++ EmployeeManagement/EmployeeManagement/Model/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Model
+{
+    public class Employee
+    {                                                                                                                                                            
+        private int userId;
+        private string name;
+        private string email;
+        private string password;
+        private string address;
+
+        public int UserId { get => this.userId; set => this.userId = value; }
+        public string Name { get => this.name; set => this.name = value; }
+        public string Email { get => this.email; set => this.email = value; }
+        public string Password { get => this.password; set => this.password = value; }
+        public string Address { get => this.address; set => this.address = value; }
+    }
+}
---
+++ EmployeeManagement/EmployeeManagement/Model/EmployeeData.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Model
+{
+    using EmployeeManagement.Utility;
+    using System.Data;
+    using System.Data.SqlClient;
+
+    public class EmployeeData
+    {
+        string connection = ConnectionString.ConnectionName;
+
+        public IEnumerable<Employee> GetAllEmployee()
+        {
+            List<Employee> list = new List<Employee>();
+            using(SqlConnection connect = new SqlConnection(connection))
+            {
+                SqlCommand cmd = new SqlCommand("spGetAllEmployee", connect);
+                cmd.CommandType = CommandType.StoredProcedure;
+                connect.Open();
+                SqlDataReader data = cmd.ExecuteReader();
+
+                while (data.Read())
+                {
+                    Employee employee = new Employee();
+                    employee.UserId = Convert.ToInt32(data["userId"]);
+                    employee.Name = data["name"].ToString();
+                    employee.Email = data["email"].ToString();
+                    employee.Password = data["password"].ToString();
+                    employee.Address = data["address"].ToString();
+
+                    list.Add(employee);
+                }
+
+                connect.Close();
+            }
+
+            return list;
+        }
+
+        public void AddEmployee(Employee employee)
+        {
+            using (SqlConnection connect = new SqlConnection(connection))
+            {
+                SqlCommand cmd = new SqlCommand("spGetAllEmployee", connect);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@name", employee.Name);
+                cmd.Parameters.AddWithValue("@email", employee.Email);
+                cmd.Parameters.AddWithValue("@password", employee.Password);
+                cmd.Parameters.AddWithValue("@address", employee.Address);
+
+                connect.Open();
+                cmd.ExecuteNonQuery();
+                connect.Close();
+            }
+        }
+
+        public void UpdateEmployee(Employee employee)
+        {
+            using (SqlConnection connect = new SqlConnection(connection))
+            {
+                SqlCommand cmd = new SqlCommand("spGetAllEmployee", connect);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@name", employee.Name);
+                cmd.Parameters.AddWithValue("@email", employee.Email);
+                cmd.Parameters.AddWithValue("@password", employee.Password);
+                cmd.Parameters.AddWithValue("@address", employee.Address);
+
+                connect.Open();
+                cmd.ExecuteNonQuery();
+                connect.Close();
+            }
+        }
+    }
+}
