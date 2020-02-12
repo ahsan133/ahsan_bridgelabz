9da290c7d2f97c3cff8d8bb5455bfe735e047d6b
ahsan133 <mahsanullah512@gmail.com>
2020-02-12 09:56:15 +0530

    [MD Ahsanullah] Add. login page is properly alligned and social login icons are added

--- Angular_6/src/app/app.module.ts
+++ Angular_6/src/app/app.module.ts
@@ -1,17 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-// import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatIconModule} from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-import { ForgotComponent } from './forgot/forgot.component';
+import { LoginComponent } from './Component/login/login.component';
+import { RegisterComponent } from './Component/register/register.component';
+import { ForgotComponent } from './Component/forgot/forgot.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    // MatFormFieldModule,
     LoginComponent,
     RegisterComponent,
     ForgotComponent
@@ -19,9 +24,18 @@ import { ForgotComponent } from './forgot/forgot.component';
 
   imports: [
     BrowserModule,
-    AppRoutingModule
+    MatFormFieldModule,
+    MatDialogModule,
+    MatIconModule,
+    AppRoutingModule,
+    MatCardModule,
+    MatInputModule,
+    BrowserAnimationsModule,
+    // ReactiveFormsModule,
+    // FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+export class MyModule {}
