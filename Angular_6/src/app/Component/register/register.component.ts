b2bcf0e8e8b152cb2a40e8dd5c4135861f0e813c
ahsan133 <mahsanullah512@gmail.com>
2020-02-13 12:42:17 +0530

    [MD Ahsanullah] Add. imported the account.service and router to register.ts

--- Angular_6/src/app/Component/register/register.component.ts
+++ Angular_6/src/app/Component/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from 'src/app/Services/account.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
