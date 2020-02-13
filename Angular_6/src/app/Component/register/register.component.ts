3c7f503b7482890e563577c7b6a3010b2ee0a426
ahsan133 <mahsanullah512@gmail.com>
2020-02-13 19:31:33 +0530

    [MD Ahsanullah] Add. FormGroup for register is added

--- Angular_6/src/app/Component/register/register.component.ts
+++ Angular_6/src/app/Component/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { AccountService } from 'src/app/Services/account.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { FormControl, Validators, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -8,16 +9,25 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  RegisterForm: FormGroup;
 
-  @Input() Data ={firstName:'', lastName:'', email:'', password:''};
 
-  constructor(public account: AccountService, private route: ActivatedRoute, private router: Router) { }
+
+
+  constructor(public account: AccountService, private route: ActivatedRoute, private router: Router) {
+    this.RegisterForm = new FormGroup({
+      firstName: new FormControl('', Validators.required),
+      lastName: new FormControl('', Validators.required),
+      email: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.required)
+    });
+   }
 
   ngOnInit(): void {
   }
 
   addProduct() {
-    this.account.addProduct(this.Data).subscribe((result) => {
+    this.account.addProduct(this.RegisterForm).subscribe((result) => {
       this.router.navigate(['/']);
     }, (err) => {
       console.log(err);
