b699cad84c4ee1e88c7f4c2b0f93b650adb2738c
ahsan133 <mahsanullah512@gmail.com>
2020-02-29 17:40:05 +0530

    [MD Ahsanullah] Add. collaborator component is added to cardComponent

--- Angular_6/src/app/Component/card/card.component.ts
+++ Angular_6/src/app/Component/card/card.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {MatDialogRef} from '@angular/material/dialog';
-
+import {MatDialogRef, MatDialog} from '@angular/material/dialog';
+import {CollaboratorComponent} from 'src/app/Component/collaborator/collaborator.component';
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
@@ -8,13 +8,21 @@ import {MatDialogRef} from '@angular/material/dialog';
 })
 export class CardComponent implements OnInit {
 
-  constructor(public dialogRef: MatDialogRef<CardComponent>) { }
+  constructor(public dialogRef: MatDialogRef<CardComponent>, public dialog:MatDialog) { }
 
   ngOnInit() {
   }
 
   onNoClick(): void {
     this.dialogRef.close();
+}
+
+AddCollaborator(): void{
+  const dialogRef =this.dialog.open(CollaboratorComponent ,{ width: '370px'});
+
+  dialogRef.afterClosed().subscribe(result => {
+    console.log('The dialog was closed');
+  })
 }
 
 }
--- Angular_6/src/app/Component/notes/notes.component.ts
+++ Angular_6/src/app/Component/notes/notes.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {MatDialog,} from '@angular/material/dialog';
+import {MatDialog} from '@angular/material/dialog';
 import {CollaboratorComponent} from 'src/app/Component/collaborator/collaborator.component';
 import {CardComponent} from 'src/app/Component/card/card.component';
 
