f9e97c495d19d1d862c1f762f362923be4f24994
ahsan133 <mahsanullah512@gmail.com>
2020-04-21 18:14:35 +0530

    [MD Ahsanullah] Add. selenium test case for user register is added

--- FundooApp/SeliniumTesting/UserTest.cs
+++ FundooApp/SeliniumTesting/UserTest.cs
@@ -14,6 +14,17 @@ namespace SeliniumTesting
 
         }
 
+       
+        [Test]
+        public void Register()
+        {
+            driver.Navigate().GoToUrl("http://localhost:4200/register");
+            driver.FindElement(By.Id("mat-input-0")).SendKeys("mda");
+            driver.FindElement(By.Id("mat-input-1")).SendKeys("ahsan");
+            driver.FindElement(By.Id("mat-input-2")).SendKeys("ahsan@gmail.com");
+            driver.FindElement(By.Id("mat-input-3")).SendKeys("ahsan123");
+            driver.FindElement(By.Id("btn")).Click();
+        }
 
         [Test]
         public void Login()
@@ -22,9 +33,15 @@ namespace SeliniumTesting
             driver.FindElement(By.Id("mat-input-0")).SendKeys("ahsan@gmail.com");
             driver.FindElement(By.Id("mat-input-1")).SendKeys("ahsan123");
             driver.FindElement(By.Id("btn")).Click();
-
         }
 
+        [Test]
+        public void Forgot()
+        {
+            driver.Navigate().GoToUrl("http://localhost:4200/forgot");
+            driver.FindElement(By.Id("mat-input-0")).SendKeys("ahsan@gmail.com");
+            driver.FindElement(By.Id("btn")).Click();
+        }
 
         [TearDown]
         public void CleanUp()
