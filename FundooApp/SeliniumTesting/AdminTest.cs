0bd679907c2eed88fa3e50927110abba062b0cd2
ahsan133 <mahsanullah512@gmail.com>
2020-04-21 18:36:13 +0530

    [MD Ahsanullah] Add. selenium test case for admin is added

--- FundooApp/SeliniumTesting/AdminTest.cs
+++ FundooApp/SeliniumTesting/AdminTest.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +9,33 @@ namespace SeliniumTesting
 {
     class AdminTest
     {
+        IWebDriver driver = new ChromeDriver();
+
+        [Test]
+        public void AdminRegister()
+        {
+            driver.Navigate().GoToUrl("http://localhost:4200/adminRegister");
+            driver.FindElement(By.Id("mat-input-0")).SendKeys("mda");
+            driver.FindElement(By.Id("mat-input-1")).SendKeys("ahsan");
+            driver.FindElement(By.Id("mat-input-2")).SendKeys("ahsan@gmail.com");
+            driver.FindElement(By.Id("mat-input-3")).SendKeys("ahsan123");
+            driver.FindElement(By.Id("btn")).Click();
+        }
+
+        [Test]
+        public void AdminLogin()
+        {
+            driver.Navigate().GoToUrl("http://localhost:4200/login");
+            driver.FindElement(By.Id("adminLogin")).Click();
+            driver.FindElement(By.Id("mat-input-2")).SendKeys("ahsan@gmail.com");
+            driver.FindElement(By.Id("mat-input-3")).SendKeys("ahsan123");
+            driver.FindElement(By.Id("btn")).Click();
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
+            driver.Close();
+        }
     }
 }
