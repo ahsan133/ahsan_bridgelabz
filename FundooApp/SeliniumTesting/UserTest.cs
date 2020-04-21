6231f0cd8468ee6ed8af947f1f9d73eca62e31ea
ahsan133 <mahsanullah512@gmail.com>
2020-04-21 17:02:49 +0530

    [MD Ahsanullah] Add. Selenium test case for login is added

---
+++ FundooApp/SeliniumTesting/UserTest.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeliniumTesting
+{
+    class UserTest
+    {
+        IWebDriver driver = new ChromeDriver();
+
+        [SetUp]
+        public void Initialize()
+        {
+
+        }
+
+
+        [Test]
+        public void Login()
+        {
+            driver.Navigate().GoToUrl("http://localhost:4200/login");
+            driver.FindElement(By.Id("mat-input-0")).SendKeys("ahsan@gmail.com");
+            driver.FindElement(By.Id("mat-input-1")).SendKeys("ahsan123");
+            driver.FindElement(By.Id("btn")).Click();
+
+        }
+
+
+        [TearDown]
+        public void CleanUp()
+        {
+            driver.Close();
+        }
+    }
+}
